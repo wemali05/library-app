19772f85fc3a5d0659449bb75cac094c8516cdcc
wemali05 <alfredwemali05@gmail.com>
2025-03-23 18:23:14 +0300

    setup app resources

---
+++ app/Models/Category.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    /** @use HasFactory<\Database\Factories\CategoryFactory> */
+    use HasFactory;
+
+    /**
+     * @return HasMany
+     */
+    public function books() {
+        return $this->hasMany(Books::class);
+    }
+}
