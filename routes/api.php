59d41cd9e35c23e8b3f559642b43eb67fbe280d2
wemali05 <alfredwemali05@gmail.com>
2025-03-24 06:20:00 +0300

    auth APIs

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::group(['middleware' => ['json.response']], function () {
+    Route::prefix('auth')->group(base_path('routes/auth.php'));
+});
